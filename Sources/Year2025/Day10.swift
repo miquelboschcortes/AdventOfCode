24466c1c3ea243501ce5f80380b769b61d1351e8
Miquel Bosch <miquel.bosch@thetrainline.com>
2025-12-10 20:43:57 +0100

    added BFS logic to calculate min presses

--- Sources/Year2025/Day10.swift
+++ Sources/Year2025/Day10.swift
@@ -9,7 +9,7 @@ struct Day10: Day {
     
     struct Machine {
         let target: [Int]
-        let positions: [[Int]]
+        let buttons: [[Int]]
     }
     
     func extractTarget(_ line: String) -> [Int] {
@@ -19,7 +19,7 @@ struct Day10: Day {
             .map { $0 == "#" ? 1 : 0 } ?? []
     }
     
-    func extractTargetAndPositions(_ input: String) {
+    func extractTargetAndPositions(_ input: String) -> [Machine] {
         
         var machines: [Machine] = []
         
@@ -42,7 +42,6 @@ struct Day10: Day {
             var positions: [[Int]] = []
             
             for match in line.matches(of: positionRegex) {
-//                print(match.output.1)
                 let numbers = match.output.1
                     .trimmingCharacters(in: .whitespaces)
                     .split(separator: ",")
@@ -51,16 +50,79 @@ struct Day10: Day {
                 positions.append(numbers)
             }
             
-            machines.append(Machine(target: extractTarget(line), positions: positions))
-            
+            machines.append(
+                Machine(
+                    target: extractTarget(line),
+                    buttons: positions
+                )
+            )
         }
-        print(machines)
+        
+        return machines
+    }
+    
+    func encodeState(_ state: [Int]) -> String {
+        String(state.map { Character("\($0)") })
+    }
+    
+    func toggledState(_ currentState: [Int], using button: [Int]) -> [Int] {
+        var result = currentState
+        for i in button {
+            result[i] = 1 - result[i]
+        }
+        return result
+    }
+    
+    func minPressesBFS(for machine: Machine) -> Int {
+        let target = machine.target
+        let buttons = machine.buttons
+        let targetLength = target.count
+        
+        let start = Array(repeating: 0, count: targetLength)
+        
+        if start == target {
+            return 0
+        }
+        
+        var queue: [(state: [Int], distance: Int)] = []
+        var head = 0
+        
+        var visited: Set<String> = []
+        let startKey = encodeState(start)
+        
+        queue.append((start, 0))
+        visited.insert(startKey)
+        
+        while head < queue.count {
+            
+            let (currentState, distance) = queue[head]
+            head += 1
+            
+            for button in buttons {
+                let next = toggledState(currentState, using: button)
+                
+                if next == target {
+                    return distance + 1
+                }
+                
+                let key = encodeState(next)
+                if !visited.contains(key) {
+                    visited.insert(key)
+                    queue.append((next, distance + 1))
+                }
+            }
+        }
+        
+        return -1
+    }
+    
+    func totalMinPresses(for machines: [Machine]) -> Int {
+        machines.reduce(0) { $0 + minPressesBFS(for: $1) }
     }
     
     func part1() -> String {
-        
-        extractTargetAndPositions(input)
-        return "Not implemented"
+        let machines = extractTargetAndPositions(input)
+        return String(totalMinPresses(for: machines))
     }
     
     func part2() -> String {
